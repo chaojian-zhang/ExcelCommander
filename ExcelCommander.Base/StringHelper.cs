c347dc750224307d0e79e1f9fb50a3664110583a
Chaojian Zhang <szinubuntu@gmail.com>
2023-06-05 16:01:00 -0400

    [Base] Add StringHelper for parsing parameters

---
+++ ExcelCommander.Base/StringHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcelCommander.Base
+{
+    public static class StringHelper
+    {
+        public static string[] SplitParameters(this string inputString)
+        {
+            List<string> parameters = new List<string>();
+            StringBuilder current = new StringBuilder();
+
+            bool inQuotes = false;
+            foreach (var c in inputString)
+            {
+                switch (c)
+                {
+                    case '"':
+                        inQuotes = !inQuotes;
+                        break;
+                    case ' ':
+                        if (!inQuotes)
+                        {
+                            parameters.Add(current.ToString());
+                            current.Clear();
+                        }
+                        else
+                            current.Append(c);
+                        break;
+                    default:
+                        current.Append(c);
+                        break;
+                }
+            }
+            if (current.Length != 0)
+                parameters.Add(current.ToString());
+            return parameters.ToArray();
+        }
+    }
+}
---
+++ ExcelCommanderUnitTests/StringHelperTest.cs
@@ -0,0 +1,15 @@
+﻿using ExcelCommander.Base;
+
+namespace ExcelCommanderUnitTests
+{
+    public class StringHelperTest
+    {
+        [Fact]
+        public void ParametersSplitShouldConsiderQuotes()
+        {
+            Assert.Equal(2, "hello world".SplitParameters().Length);
+            Assert.Equal(2, "Command \"Argument 1\"".SplitParameters().Length);
+            Assert.Equal("Argument 2", "Command \"Argument 1\" \"Argument 2\"".SplitParameters().Last());
+        }
+    }
+}
