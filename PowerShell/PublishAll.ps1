0328728cb29e7374c61b66e4b451adeb907632a9
Chaojian Zhang <szinubuntu@gmail.com>
2023-06-05 17:04:25 -0400

    [Automation] Create publish script

---
+++ PowerShell/PublishAll.ps1
@@ -0,0 +1,60 @@
+$PrevPath = Get-Location
+
+Write-Host "Publish for Final Packaging build."
+Set-Location $PSScriptRoot
+
+$PublishFolder = "$PSScriptRoot\..\Publish"
+$LibraryPublishFolder = "$PublishFolder\Libraries"
+$NugetPublishFolder = "$PublishFolder\Nugets"
+
+# Delete current data
+Remove-Item $PublishFolder -Recurse -Force
+
+# Publish Executables
+$PublishExecutables = @(
+    "ExcelCommander"
+)
+foreach ($Item in $PublishExecutables)
+{
+    dotnet publish $PSScriptRoot\..\$Item\$Item.csproj --use-current-runtime --output $PublishFolder
+}
+# Publish Windows-only Executables
+$PublishWindowsExecutables = @(
+)
+foreach ($Item in $PublishWindowsExecutables)
+{
+    dotnet publish $PSScriptRoot\..\$Item\$Item.csproj --runtime win-x64 --self-contained --output $PublishFolder
+}
+# Publish Loose Libraries
+$PublishLibraries = @(
+    "ExcelCommander.Addin"
+    "ExcelCommander.Base"
+)
+foreach ($Item in $PublishLibraries)
+{
+    dotnet publish $PSScriptRoot\..\$Item\$Item.csproj --use-current-runtime --output $LibraryPublishFolder
+}
+# Publish Nugets
+$PublishNugets = @(
+    "ExcelCommander"
+)
+foreach ($Item in $PublishNugets)
+{
+    dotnet pack $PSScriptRoot\..\$Item\$Item.csproj --output $NugetPublishFolder
+}
+
+# Create archive
+$Date = Get-Date -Format yyyyMMdd
+$ArchiveFolder = "$PublishFolder\..\Packages"
+$ArchivePath = "$ArchiveFolder\ExcelCommander_DistributionBuild_Windows_B$Date.zip"
+New-Item -ItemType Directory -Force -Path $ArchiveFolder
+Compress-Archive -Path $PublishFolder\* -DestinationPath $ArchivePath -Force
+
+# Validation
+if (-Not (Test-Path (Join-Path $PublishFolder "ExcelCommander.exe")))
+{
+    Write-Host "Build failed."
+    Exit
+}
+
+Set-Location $PrevPath
